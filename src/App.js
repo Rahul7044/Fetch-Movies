74792f0ef12b9696d54d0c19404a457fc043ad6d
Rahul7044 <rahulkumar581999@gmail.com>
2023-04-19 15:08:35 +0530

    showing loading using state

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import MoviesList from "./components/MoviesList";
 import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const FetchMovieHandler =  async () => {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
         const transformedMovies = data.results.map((movie) => {
@@ -16,6 +18,7 @@ function App() {
           };
         });
         setMovies(transformedMovies);
+        setIsLoading(false);
 
   };
 
@@ -25,7 +28,9 @@ function App() {
         <button onClick={FetchMovieHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        { !isLoading &&movies.length>0 && <MoviesList movies={movies} /> }
+        {!isLoading && movies.length === 0 && <p>Found no movies</p>}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
